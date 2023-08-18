7b7f605d3c2916396a4fd2461d80b78fffa89fcf
Coding-Jim <75325097+Coding-Jim@users.noreply.github.com>
2023-08-18 11:24:32 +0200

    Guest Book Upload

---
+++ HomeWorkMiniProjectBetterGuestBookApp/HomeWorkMiniProjectBetterGuestBook/GetGuestInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWorkMiniProjectBetterGuestBook;
+
+public class GetGuestInfo
+{
+    public static string GetInfo(string message)
+    {
+        Console.Write(message);
+        string output = Console.ReadLine();
+
+        return output;
+    }
+
+
+}
